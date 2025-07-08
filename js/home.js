c11e10cf1cf2576a52091d68f9ea81a5906e86a5
Maharani Margareth Massie Hihola <maharanihihola106@student.unsrat.ac.id>
2025-07-08 17:14:53 +0800

    edit navbar

--- js/home.js
+++ js/home.js
@@ -2,13 +2,13 @@ const menuBtn = document.getElementById("menuToggle");
 const sidebar = document.getElementById("sidebar");
 const navbar = document.getElementById("navbar");
 
-// Toggle sidebar
+
 menuBtn.addEventListener("click", () => {
 menuBtn.classList.toggle("active");
 sidebar.classList.toggle("show");
 });
 
-// Add shadow on scroll
+
 window.addEventListener("scroll", () => {
 if (window.scrollY > 20) {
     navbar.classList.add("scrolled");

fba02dcc4b60a1e82a103962a881f017a072592f
Maharani Margareth Massie Hihola <maharanihihola106@student.unsrat.ac.id>
2025-07-08 18:41:43 +0800

    update trasisi icon sidebar

--- js/home.js
+++ js/home.js
@@ -3,9 +3,24 @@ const sidebar = document.getElementById("sidebar");
 const navbar = document.getElementById("navbar");
 
 
+const menuIcon = document.getElementById("menuIcon");
+
 menuBtn.addEventListener("click", () => {
-menuBtn.classList.toggle("active");
-sidebar.classList.toggle("show");
+    menuBtn.classList.toggle("active");
+    sidebar.classList.toggle("show");
+
+    menuIcon.classList.add("icon-rotate");
+
+    setTimeout(() => {
+        if (menuBtn.classList.contains("active")) {
+        menuIcon.classList.remove("fa-bars-staggered");
+        menuIcon.classList.add("fa-xmark");
+        } else {
+        menuIcon.classList.remove("fa-xmark");
+        menuIcon.classList.add("fa-bars-staggered");
+        }
+        menuIcon.classList.remove("icon-rotate");
+    }, 200);
 });
 
 
